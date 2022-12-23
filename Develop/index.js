b159dcc20cd30fd00ef6eb3ad3d6ba923a4dfe4a
Gabe Morris <gabemorris1042@gmail.com>
2022-12-23 10:47:06 -0400

    installed inquirer, got writefile to edit the current README with the users data from command line.

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: `Please give a description for your project.`
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: `Please provide installation instructions required for your project.`
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: `Enter any relevant usage information for your project.`
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: `Enter your project's contribution guidelines.`
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: `Please provide test instructions for your project.`
+    }
+  ])
+  .then((data) => {
+    // console.log(data)
+
+    const filename = 'README.md';
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  })
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
